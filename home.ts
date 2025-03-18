8f7b33075f1db107d9420e431ba38a8f197f5bbb
Mark Lee <mlee.aus@gmail.com>
2025-03-18 15:50:13 +1100

    wip: postgres init and docker-compose config

---
+++ home.ts
@@ -0,0 +1,10 @@
+export function GetHome() {
+  return `<!DOCTYPE html>
+    <html>
+      <head><title>Hello oak!</title><head>
+      <body>
+        <h1>Hello Mark!</h1>
+      </body>
+    </html>
+  `
+}
--- main.ts
+++ main.ts
@@ -1,9 +1,36 @@
-import { Hono } from 'hono'
+import { Application } from 'jsr:@oak/oak/application'
+import { Router } from 'jsr:@oak/oak/router'
+import cfg from './config/config.ts'
+import { GetHome } from './home.ts'
 
-const app = new Hono()
+const router = new Router()
+const app = new Application()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(async (ctx, next) => {
+  await next()
+  const rt = ctx.response.headers.get('X-Response-Time')
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`)
 })
 
-Deno.serve(app.fetch)
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  ctx.response.headers.set('X-Response-Time', `${ms}ms`)
+})
+
+router.get('/', (ctx) => {
+  console.log(ctx.request.ip)
+  ctx.response.body = GetHome
+})
+
+const port = cfg.port
+const cert = await Deno.readTextFile(cfg.cert)
+const key = await Deno.readTextFile(cfg.key)
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+console.log(`Server running at http://localhost:${port}`)
+app.listen({ port, cert, key })
