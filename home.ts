776a543b4660152a33568b78f91cadf097f6c2f4
Mark Lee <mlee.aus@gmail.com>
2025-03-19 15:48:25 +1100

    wip: add bootstrap and base home page

--- home.ts
+++ home.ts
@@ -1,10 +1,22 @@
 export function GetHome() {
-  return `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <h1>Hello Mark!</h1>
-      </body>
-    </html>
-  `
+  return `<!doctype html>
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="A web application that does school stuff.">
+  <title>SUBSUBS</title>
+  <link rel="icon"
+    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐦</text></svg>">
+  <link rel="stylesheet" type="text/css" href="/bootstrap-5.3.3-dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+  <script src="/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js" defer></script>
+</head>
+<body>
+<div class="container-fluid">
+  <h1>Some content</h1>
+  <p>Some more content</p>
+</div>
+</body>
+</html>`
 }
