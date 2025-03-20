83c1ec2a2e78a74cedf670a553ed8389e6b3f74c
Mark Lee <mlee.aus@gmail.com>
2025-03-20 15:18:08 +1100

    wip

--- home.ts
+++ home.ts
@@ -5,18 +5,31 @@ export function GetHome() {
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="A web application that does school stuff.">
-  <title>SUBSUBS</title>
+  <title>SubDubs</title>
   <link rel="icon"
-    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐦</text></svg>">
+    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📺</text></svg>">
   <link rel="stylesheet" type="text/css" href="/bootstrap-5.3.3-dist/css/bootstrap.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
   <script src="/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js" defer></script>
 </head>
 <body>
-<div class="container-fluid">
-  <h1>Some content</h1>
-  <p>Some more content</p>
-</div>
+  <div class="container-fluid">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <img
+          width="200"
+          src="/img/CHELTSEC-LOGO-RGB-COLOUR-POSITIVE.SVG"
+          alt="csc-logo" />
+      </a>
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+  </div>
 </body>
 </html>`
 }
